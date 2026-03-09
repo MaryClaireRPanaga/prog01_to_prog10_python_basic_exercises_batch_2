084dea0ec06fac40aeea29a0d4206bfef2bd80b6
MaryClaireRPanaga <maryclaireremigiopanaga@gmail.com>
2026-03-09 19:55:58 +0800

    Delete prog06_subtract_remaining_numbers.py

--- prog06_subtract_remaining_numbers.py
+++
@@ -1,8 +0,0 @@
-# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
-# Input the first number
-result = int(input("Enter number 1: "))
-# Input the remaining 9 numbers
-for i in range(2,11):
-    num = int(input("Enter number {i}: "))
-    result -= num # Subtract the current number from the result
-print("Result:", result)
