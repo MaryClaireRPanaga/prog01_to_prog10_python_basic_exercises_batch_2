8de249a0ad6b436babb31bed7f3dd12d7e7d2efe
MaryClaireRPanaga <maryclaireremigiopanaga@gmail.com>
2026-03-09 19:02:51 +0800

    implemented_check_to_print_message_when_inputs_differ

---
+++ prog02_check_not_equal.py
@@ -0,0 +1,5 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+if num1 != num2:
+    print("Not Equal")
