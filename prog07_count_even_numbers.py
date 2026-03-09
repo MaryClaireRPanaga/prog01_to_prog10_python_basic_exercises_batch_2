228827d3d1bc2d7bdcb951f3d0d6e29bf5e0e277
MaryClaireRPanaga <maryclaireremigiopanaga@gmail.com>
2026-03-09 19:59:10 +0800

    added_counter_to_track_even_numbers_in_ten_input_sequence

---
+++ prog07_count_even_numbers.py
@@ -0,0 +1,7 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+even_count = 0
+for i in range(1,11):
+    num = int(input(f"Enter number {i}: "))
+    if num % 2 == 0:
+        even_count += 1
+print("Number of even numbers:", even_count)
